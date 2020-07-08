03b2c6969afd09c0c7e13468ec2ccce87c5787a5
pedropimont <pedro.pimontf@gmail.com>
2020-07-08 18:52:06 -0300

    Add GameName to InitialView
    Create GameName Class and Config file

--- lib/paths.dart
+++ lib/paths.dart
@@ -1,3 +1,6 @@
 library paths;
 
 const String background = 'background.png';
+const String gameName = 'views/initial/gameName.png';
+const String logoArt = 'views/initial/logoArt.png';
+const String startButton = 'views/initial/startButton.png';

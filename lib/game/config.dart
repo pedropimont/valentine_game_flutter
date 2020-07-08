b0ccd57d4929b223586c7b9584dc544bd43d673c
pedropimont <pedro.pimontf@gmail.com>
2020-07-08 19:36:45 -0300

    Add missing files
    Some files that didn't integrate on last push.

---
+++ lib/game/config.dart
@@ -0,0 +1,12 @@
+library configs;
+
+// Initial View
+//    GameName -> Anchor: Top Center
+const double gameNameWidthPortion = 3 / 4; // 75% of screen width
+const double gameNameDimensionRatio = 438 / 195; // w x h (Standard)
+const double gameNameY = 1 / 4; // 25% Screen Height from Top
+
+//    Start Button -> Anchor: Top Center
+const double startButtonWidthPortion = 2 / 3;
+const double startButtonDimensionRatio = 628 / 87;
+const double startButtonY = 3 / 4;
---
+++ lib/game/views/initial/startButton.dart
@@ -0,0 +1,34 @@
+import 'dart:ui';
+
+import 'package:flame/anchor.dart';
+import 'package:flame/components/component.dart';
+import 'package:flame/components/mixins/resizable.dart';
+import 'package:flame/components/mixins/tapable.dart';
+import 'package:flame/sprite.dart';
+import 'package:flutter/gestures.dart';
+
+import '../../game.dart';
+import '../../../paths.dart' as Path;
+import '../../config.dart' as Config;
+
+class StartButton extends SpriteComponent with Resizable, Tapable {
+  ValentineGame game;
+
+  StartButton(this.game)
+      : super.fromSprite(16.0, 16.0, Sprite(Path.startButton)) {
+    anchor = Anchor.topCenter;
+  }
+
+  @override
+  void onTapUp(TapUpDetails details) {
+    game.start();
+  }
+
+  @override
+  void resize(Size size) {
+    x = size.width / 2;
+    y = size.height * Config.startButtonY;
+    width = size.width * Config.startButtonWidthPortion;
+    height = width / Config.startButtonDimensionRatio;
+  }
+}
